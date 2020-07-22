138d10f3ee33183cdb56804056559de2c492dca0
kevinjang <kevinjang@163.com>
2020-07-22 16:30:16 +0800

    Update index.js
    主页清空。

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,12 @@ function App1(props) {
   return (
     <div className={styles.normal}>
       {/* <div className={styles.welcome} /> */}
-      <ul className={styles.list}>
+      {/* <ul className={styles.list}>
         <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
         <li>
           <Link to="/test01">test01</Link>
         </li>
-      </ul>
+      </ul> */}
     </div>
   );
 }
